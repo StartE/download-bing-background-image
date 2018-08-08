a6cbf951f0bcc91b991df68c32119d18bb2859ff
StartE <yili0618@qq.com>
2018-08-08 16:57:00 +0800

    simple script to download bing background image

---
+++ READEME.md
@@ -0,0 +1,19 @@
+# Download Bing Background Image 
+
+A simple and elegant way to download beautiful image of [bing](https://cn.bing.com)
+
+## Requirements
+* pip install [requests](http://docs.python-requests.org/en/master/)
+
+## Example
+* set the *save_dir* of images
+```py
+import bing 
+save_dir ="C:\\Users\\XXX\\Documents\\python\\images"
+
+download_bing = bing.DownloadBingImage(save_dir)
+image_url = download_bing.get_bing_image_url()
+if image_url:
+    download_bing.save_bing_image(image_url)
+```
+* you can also set the proxy if necessary
---
+++ bing.py
@@ -0,0 +1,56 @@
+import requests
+import re
+import os.path
+
+save_dir ="C:\\Users\\yil1\\Documents\\python\\images"
+proxy= {} # proxy = {'http':'xxx.xx.com:port_num'}
+
+page_url = 'https://www.bing.com'
+response_code = {'OK':200}
+
+
+class DownloadBingImage():
+
+    def __init__(self,save_dir,page_url = page_url,proxy = {}):
+        self.save_dir = save_dir
+        self.page_url = page_url
+        self.proxy = proxy
+
+    
+    def get_bing_image_url(self):
+        load_page = requests.get(self.page_url,proxies = self.proxy)
+        if(load_page.status_code == response_code['OK']):
+            pattern = r"url:[.,/_:\s\-\"\'a-zA-Z0-9]+bgDiv"
+            search_img_url =re.search(pattern,load_page.text)
+            if search_img_url!= None:
+                url = search_img_url.group()
+                url = url.split('"')[1]
+                url = page_url + url
+                return url
+            else:
+                print('Could Not Get the Image URL')
+                return
+        else:
+            print("Network Error, Response Code is {}".format(load_page.status_code))
+            return
+    def save_bing_image(self,image_url,file_name = None):
+        load_img = requests.get(image_url,proxies = self.proxy)
+        if file_name == None:
+            file_name = image_url.split('/')[-1]
+        save_path = os.path.join(self.save_dir,file_name)
+
+        if(load_img.status_code == response_code['OK']):
+            with open(save_path,'wb') as f:
+                f.write(load_img.content)
+                print('Successfully Download the Beautiful Image!')
+        else:
+            print("Network Error, Response Code is {}".format(load_img.status_code))
+
+def main():
+    bing = DownloadBingImage(save_dir)
+    image_url = bing.get_bing_image_url()
+    if image_url:
+        bing.save_bing_image(image_url)
+
+if __name__ == "__main__":
+    main()
