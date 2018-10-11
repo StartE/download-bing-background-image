0a38c966cfd3e5d0f5e21f32c2d0d763dc132785
StartE <yili0618@qq.com>
2018-10-11 11:49:07 +0800

    add #

--- bing.py
+++ bing.py
@@ -53,7 +53,7 @@ def main():
     if image_url:
         bing.save_bing_image(image_url)
     else:
-        print('=======I will check the url later please wait for patience===========')
+        print('#=======I will check the url later please wait for patience===========')
 
 if __name__ == "__main__":
     main()
